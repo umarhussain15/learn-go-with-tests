907e589fb022a13ad7a0722d7b4ff132d75ec836
Umar Hussain <umarhussain.work@gmail.com>
2019-11-13 13:21:49 +0100

    "go interface" used to calculate shape area.

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -16,23 +16,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangles", func(t *testing.T) {
-		rect := Rectangle{10, 20}
-		got := rect.Area()
-		want := 200.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := math.Pi * 100
-
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("Rectangles", func(t *testing.T) {
+		rect := Rectangle{10, 20}
+		checkArea(t, rect, 200.0)
+	})
+	t.Run("Circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, math.Pi*100)
 	})
 
 }
