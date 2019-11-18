7c79bb82a328a6d144c4d636a93faaed0bcd385f
Umar Hussain <umarhussain.work@gmail.com>
2019-11-18 22:04:54 +0100

    maps code updated to return error when key not found.

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,9 +5,17 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		assertError(t, err, ErrNotFound)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
@@ -15,5 +23,17 @@ func assertStrings(t *testing.T, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("expected to get an error.")
+	}
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
